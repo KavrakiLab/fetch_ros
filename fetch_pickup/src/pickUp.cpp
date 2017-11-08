2acf83040f81366c22cd828727d0576fe6a0e532
Constantinos Chamzas <chamzask@gmail.com>
2017-11-08 11:27:18 -0600

    Succesfully added Tf from pose to Head ans Heas to base_link

--- fetch_pickup/src/pickUp.cpp
+++ fetch_pickup/src/pickUp.cpp
@@ -11,13 +11,13 @@
 
 class getGrasps {
 public:
-  getGrasps(ros::NodeHandle &n) : n_(n),tfListener_(tfBuffer_) {
+  getGrasps(ros::NodeHandle &n) : n_(n), tfListener_(tfBuffer_) {
     sub_ = n_.subscribe("/detect_grasps/clustered_grasps", 3,
                         &getGrasps::callback, this);
     graspBest_ = gpd::GraspConfig();
     graspBest_.score.data = 0;
   }
-  void update(gpd::GraspConfig graspCurr) {
+  void update(gpd::GraspConfig &graspCurr) {
 
     graspBest_ = graspCurr;
     geometry_msgs::Vector3 app = graspBest_.approach;
@@ -26,24 +26,31 @@ public:
     tf::Matrix3x3 rot = tf::Matrix3x3(app.x, bin.x, axi.x, app.y, bin.y, axi.y,
                                       app.z, bin.z, axi.z);
 
-    tf::Point origin;  
-    //The bottom point on the surface object has been chosen
-    //Needs to be validated again
-    tf::pointMsgToTF(graspBest_.bottom,origin);
-    tf::Transform graspPose_ = tf::Transform(rot,origin);
+    // The bottom point on the surface object has been chosen as origin
+    // Needs to be validated again
+    tf::Point origin;
+    tf::pointMsgToTF(graspBest_.bottom, origin);
+    tf::Transform headToGrasp = tf::Transform(rot, origin);
+    printQuat(headToGrasp);
 
+    tf::StampedTransform worldToHead;
+    tf::transformStampedMsgToTF(
+        lookup("base_link", "head_camera_rgb_optical_frame"), worldToHead);
+    printQuat(worldToHead);
+
+    graspTf_ = worldToHead * headToGrasp;
+    printQuat(graspTf_);
   }
 
-  geometry_msgs::TransformStamped lookup(const std::string& base, const std::string target){
-      try{
-          return tfBuffer_.lookupTransform(base,target,ros::Time(0));
-      }
-      catch (tf2::LookupException ex)
-      {
-          ROS_ERROR("getGrasps::lookup*%s,%s):%s",base.c_str(), target.c_str(),ex.what());
-      }
+  geometry_msgs::TransformStamped lookup(const std::string &base,
+                                         const std::string &target) {
+    try {
+      return tfBuffer_.lookupTransform(base, target, ros::Time(0));
+    } catch (tf2::LookupException ex) {
+      ROS_ERROR("getGrasps::lookup:%s,%s):%s", base.c_str(), target.c_str(),
+                ex.what());
+    }
   }
-
 
   void callback(const gpd::GraspConfigList &input) {
     graspList_ = input;
@@ -53,19 +60,23 @@ public:
       graspCurr = graspList_.grasps[i];
 
       if ((graspBest_.score.data) < (graspCurr.score.data)) {
+        ROS_INFO("Grasp Where Updated");
         update(graspCurr);
       }
     }
   }
-
-  geometry_msgs::Point getBestGrasp() {
-    ROS_INFO("Sample Chosen: %f,%f,%f", graspBest_.bottom.x,
-             graspBest_.bottom.y, graspBest_.bottom.z);
-    return graspBest_.bottom;
+  void printQuat(tf::Transform tra) {
+    tf::Quaternion quat;
+    quat = tra.getRotation();
+    std::cout << "X:" << quat.getX() << std::endl;
+    std::cout << "Y:" << quat.getY() << std::endl;
+    std::cout << "Z:" << quat.getZ() << std::endl;
+    std::cout << "W:" << quat.getW() << std::endl;
   }
-  //geometry_msgs::Vector3 getApproach() { return approach_; }
-  std_msgs::Header getBestGraspHeader() { return graspHeader_; }
-  //geometry_msgs::Quaternion getGraspRot() { return graspRot_; }
+
+  // geometry_msgs::Vector3 getApproach() { return approach_; }
+  tf::Transform getGraspTf() { return graspTf_; };
+  // geometry_msgs::Quaternion getGraspRot() { return graspRot_; }
 
 private:
   ros::NodeHandle n_;
@@ -74,8 +85,7 @@ private:
   tf2_ros::TransformListener tfListener_;
   gpd::GraspConfigList graspList_;
   gpd::GraspConfig graspBest_;
-  std_msgs::Header graspHeader_;
-  tf::StampedTransform graspTf_;
+  tf::Transform graspTf_;
 };
 
 int main(int argc, char **argv) {
@@ -97,10 +107,9 @@ int main(int argc, char **argv) {
   const robot_state::JointModelGroup *joint_model_group =
       move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
 
-  move_group.setPoseReferenceFrame("/base_frame");
+  move_group.setPoseReferenceFrame("/base_link");
   namespace rvt = rviz_visual_tools;
-  moveit_visual_tools::MoveItVisualTools visual_tools(
-      "/base_frame");
+  moveit_visual_tools::MoveItVisualTools visual_tools("/base_link");
   visual_tools.deleteAllMarkers();
   visual_tools.loadRemoteControl();
   visual_tools.trigger();
@@ -121,43 +130,28 @@ int main(int argc, char **argv) {
   // We can plan a motion for this group to a desired pose for the
   // end-effector.
 
-  geometry_msgs::Point graspPoint;
-  graspPoint = grasps->getBestGrasp();
+  geometry_msgs::Pose graspPose;
+  geometry_msgs::Quaternion graspRot;
+  geometry_msgs::Point graspOrigin;
 
-  geometry_msgs::PoseStamped target_pose_stamped1;
-  geometry_msgs::Pose target_pose1;
-  geometry_msgs::Pose target_pose3;
-  geometry_msgs::Vector3 approach;
-  approach = grasps->getApproach();
-  target_pose1.orientation = geometry_msgs::Quaternion(grasps->getGraspRot());
-  target_pose3.orientation = geometry_msgs::Quaternion(grasps->getGraspRot());
+  tf::quaternionTFToMsg(grasps->getGraspTf().getRotation(), graspRot);
+  tf::pointTFToMsg(grasps->getGraspTf().getOrigin(), graspOrigin);
 
-  target_pose1.position.x = graspPoint.x - 0.3 * approach.x;
-  target_pose1.position.y = graspPoint.y - 0.3 * approach.y;
-  target_pose1.position.z = graspPoint.z - 0.3 * approach.z;
-  target_pose3.position.x = graspPoint.x - 0.1 * approach.x;
-  target_pose3.position.y = graspPoint.y - 0.1 * approach.y;
-  target_pose3.position.z = graspPoint.z - 0.1 * approach.z;
+  grasps->printQuat(grasps->getGraspTf());
 
-  target_pose_stamped1.pose = target_pose1;
-  target_pose_stamped1.header = grasps->getBestGraspHeader();
-  move_group.setPoseTarget(target_pose1);
+  graspPose.orientation = graspRot;
+  graspPose.position = graspOrigin;
 
-  geometry_msgs::Pose target_pose2;
-  target_pose2.orientation.w = 1.0;
-  target_pose2.position.x = 0.28;
-  target_pose2.position.y = -0.7;
-  target_pose2.position.z = 1.0;
   // Now, we call the planner to compute the plan and visualize it.
   // Note that we are just planning, not asking move_group
   // to actually move the robot.
-  visual_tools.publishAxisLabeled(target_pose1, "PreGrasp");
-  visual_tools.publishAxisLabeled(target_pose2, "poseFIXED");
-  visual_tools.publishAxisLabeled(target_pose3, "Grasp");
+  visual_tools.publishAxisLabeled(graspPose, "PreGrasp");
   visual_tools.trigger();
   // visual_tools.publishAxisLabeled(target_pose_stamped1,"stamped_pose1");
   std::cout << "To plan the path press Enter" << std::endl;
   std::cin.ignore();
+
+  move_group.setPoseTarget(graspPose);
   moveit::planning_interface::MoveGroupInterface::Plan my_plan;
   bool success = move_group.plan(my_plan);
   std::cout << "To execute the path press Enter" << std::endl;
