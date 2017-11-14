4f8fce231ed4602ee1a1a4c19ae8135f096ff615
Constantinos Chamzas <chamzask@gmail.com>
2017-11-14 17:02:17 -0600

    Added retrying

--- fetch_pickup/src/pickUp.cpp
+++ fetch_pickup/src/pickUp.cpp
@@ -23,8 +23,9 @@ void printTF(tf::Transform tra)
     tf::Point orig;
     quat = tra.getRotation();
     orig = tra.getOrigin();
-    std::cout << "Quat:[X,Y,Z,W]=[" << quat.getX() << ',' << quat.getY() << "," << quat.getZ()
-              << std::endl;
+    std::cout << "Quat:[X,Y,Z,W]=[" << quat.getX() << ',' << quat.getY() << "," << quat.getZ()<<","<<quat.getW()<< std::endl;
+    
+    std::cout << "Orig:[X,Y,Z]=[" << orig.getX() << ',' << orig.getY() << "," << orig.getZ()<<std::endl;
 }
 
 class getGrasps
@@ -45,7 +46,6 @@ public:
             tf::Matrix3x3(app.x, bin.x, axi.x, app.y, bin.y, axi.y, app.z, bin.z, axi.z);
 
         // The bottom point on the surface object has been chosen as origin
-        // Needs to be validated again
         tf::Point origin;
         tf::pointMsgToTF(graspCurr.bottom, origin);
         tf::Transform headToGrasp = tf::Transform(rot, origin);
@@ -65,10 +65,13 @@ public:
         }
         catch (tf2::LookupException ex)
         {
-            ROS_ERROR("getGrasps::lookup:%s,%s):%s", base.c_str(), target.c_str(), ex.what());
+            ROS_INFO("GraspClient::lookup:%s,%s):%s", base.c_str(), target.c_str(), ex.what());
         }
     }
     void reset(){
+
+
+        ROS_INFO("GraspClient:: graspBest_ was reseted");
         graspBest_.score.data =0;
     }
 
@@ -88,10 +91,9 @@ public:
             if (((graspBest_.score.data) < (graspCurr.score.data)) &&
                 (graspCurrTF.getOrigin().getZ() > 0.84))
             {
-                ROS_ERROR("Grasp Updated");
                 graspTF_ = graspCurrTF;
-                printTF(graspTF_);
                 graspBest_ = graspCurr;
+                printTF(graspTF_);
             }
         }
     }
@@ -111,7 +113,8 @@ public:
         tf::pointTFToMsg(graspTF_.getOrigin() + graspAppScale, graspMsgPose.position);
         // MUST CHANGE
         // HEURISTIC APPORACH TO CHOOSE GOOD GRASPS SHOULD CHANGE IN THE FUTURE
-        graspMsgPose.position.z = 0.88;
+        
+        if (graspMsgPose.position.z < 0.88){ graspMsgPose.position.z = 0.88;}
 
         return graspMsgPose;
     }
@@ -155,113 +158,140 @@ int main(int argc, char** argv)
 
     // We will use the :planning_scene_interface:`PlanningSceneInterface`
     // class to add and remove collision objects in our "virtual: world" scene
-    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-       move_group.setPoseReferenceFrame(BASE_FRAME);
+    //moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+    move_group.setPoseReferenceFrame(BASE_FRAME);
     namespace rvt = rviz_visual_tools;
     moveit_visual_tools::MoveItVisualTools visual_tools(BASE_FRAME);
     visual_tools.deleteAllMarkers();
     visual_tools.loadRemoteControl();
     visual_tools.trigger();
 
-    ROS_INFO_NAMED("tutorial", "Reference frame: %s", move_group.getPlanningFrame().c_str());
-    ROS_INFO_NAMED("tutorial", "setPoseReferenceFrame %s",
+    ROS_INFO_NAMED("GraspDemo", "Reference frame: %s", move_group.getPlanningFrame().c_str());
+    ROS_INFO_NAMED("GraspDemo", "setPoseReferenceFrame %s",
                    move_group.getPoseReferenceFrame().c_str());
 
-    ROS_INFO_NAMED("tutorial", "End effector link: %s", move_group.getEndEffectorLink().c_str());
+    ROS_INFO_NAMED("GraspDemo", "End effector link: %s", move_group.getEndEffectorLink().c_str());
 
     // waiting for a grasp to be received
-    geometry_msgs::Pose objGrasp; 
     geometry_msgs::Pose preGrasp; 
     geometry_msgs::Pose finGrasp;
     
-    bool execute_pre_grasp = true;
-    while(!execute_pre_grasp){
-    grasps->reset();
+    bool attemp_grasp =true;
+    bool execute_pre_grasp;
+    bool execute_grasp;
+    bool success_plan;
+    int num_tries; 
+    //This is the main Loop that tries to execute a grasp from start to end
+    while (attemp_grasp){
+        execute_pre_grasp = false;
+        num_tries = 0;
+        //This loop tries to execute the pregrasping
+        while(!execute_pre_grasp){
 
-    std::cout << "Waiting For Grasps to Be Received To Continue Press Enter" << std::endl;
-    std::cin.ignore();
+            grasps->reset();
+            visual_tools.deleteAllMarkers();
 
+            std::cout << "To visualize the current Best Grasp  Press Enter" << std::endl;
+            std::cin.ignore();
 
-    geometry_msgs::Pose objGrasp = grasps->getGraspPoseMsg();
-    geometry_msgs::Pose preGrasp = grasps->getGraspPoseMsg(0.3);
-    geometry_msgs::Pose finGrasp = grasps->getGraspPoseMsg(0.14);
+            preGrasp = grasps->getGraspPoseMsg(0.3);
+            finGrasp = grasps->getGraspPoseMsg(0.14);
 
-    visual_tools.publishAxisLabeled(objGrasp, "OnObject");
-    visual_tools.publishAxisLabeled(preGrasp, "preGrasp");
-    visual_tools.publishAxisLabeled(finGrasp, "Grasp");
-    visual_tools.trigger();
-    std::cout << "To plan the path press Enter" << std::endl;
-    std::cin.ignore();
+            visual_tools.publishAxisLabeled(preGrasp, "preGrasp");
+            visual_tools.publishAxisLabeled(finGrasp, "Grasp");
+            visual_tools.trigger();
+            std::cout << "To plan the path for preGrap press Enter" << std::endl;
+            std::cin.ignore();
 
-    // Setting  a Pose goal for the move_group. In this case the arm
-    move_group.setPoseTarget(preGrasp);
-    // Now, we call the planner to compute the plan .
-    moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-    bool successPlan = move_group.plan(my_plan);
-   
+            // Setting  a Pose goal for the move_group. In this case the arm
+            move_group.setPoseTarget(preGrasp);
+            // Now, we call the planner to compute the plan .
+            moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+            success_plan = move_group.plan(my_plan);
+           
 
-    //To execute the plan we must use the execute(). Note move() both plans and executes
-    if (successPlan){
-    std::cout << "To execute the path press Enter" << std::endl;
-    std::cin.ignore();
-    bool execute_pre_grasp = move_group.execute(my_plan);
+            //To execute the plan we must use the execute(). Note move() both plans and executes
+            if (success_plan){
+                std::cout << "To execute the path for preGrasp press Enter" << std::endl;
+                std::cin.ignore();
+                num_tries = 0;
+                while (num_tries<5&&!execute_pre_grasp){
+                        execute_pre_grasp = move_group.execute(my_plan);
+                        num_tries+=1;
+                }
+            }
+            else {
+                ROS_INFO_NAMED("graspDemo", "Failed to find plan. Retrying ...");
+            }
+
+        }
+
+        execute_grasp = false; 
+        //This loop will try to execute the final grasping. Can only be executed if pre_grasping was a success.
+        while(!execute_grasp&&execute_pre_grasp){
+            
+            std::cout << "To find the trajectory for  Grasp Press Enter" << std::endl;
+            std::cin.ignore();
+            std::vector<geometry_msgs::Pose> waypoints;
+            waypoints.push_back(preGrasp);
+            waypoints.push_back(finGrasp);
+            moveit_msgs::RobotTrajectory trajectory;
+            const double jthr= 0;
+            const double eef_step= 0.3;
+            double fraction =  0;
+            num_tries = 0;
+            while (fraction <0.8&& num_tries<5){
+                fraction = move_group.computeCartesianPath(waypoints, eef_step, jthr, trajectory,false);
+                num_tries+=1;
+            }
+            if (fraction <0.8){execute_pre_grasp = false;}
+            if (execute_pre_grasp){
+
+                std::cout << "To Open Gripper Press Enter" << std::endl;
+                std::cin.ignore();
+                ROS_INFO("Openning gripper ..");
+                control_msgs::GripperCommandGoal gripperGoal;
+                control_msgs::GripperCommand gripperCommand;
+                gripperCommand.position = 0.1; 
+                gripperCommand.max_effort = -1; 
+                gripperGoal.command = gripperCommand;
+                gripperClient.sendGoal(gripperGoal);
+                bool gripSuccess=  gripperClient.waitForResult();
+                if (gripSuccess){ROS_INFO_NAMED("graspDemo","Gripper Oppened Succesfully");}
+                else{ ROS_ERROR("Gripper Failed To open please override Manually");}
+
+                std::cin.ignore();
+                control_msgs::FollowJointTrajectoryGoal trajGoal;
+                trajGoal.trajectory = trajectory.joint_trajectory;
+                armClient.sendGoal(trajGoal);
+                while (num_tries<5&&!execute_grasp){
+                        execute_grasp =  armClient.waitForResult();
+                        num_tries+=1;
+                }
+                if (execute_grasp){ROS_INFO("ACTION WAS EXECUTED SUCCESFULY!!!!!!");}
+                else{ ROS_ERROR("Execution failed ");}
+
+                std::cout << "To close the  Gripper Press Enter" << std::endl;
+                std::cin.ignore();
+
+                ROS_INFO("Closing  gripper ..");
+                gripperCommand.position = 0.02; 
+                gripperCommand.max_effort = -1; 
+                gripperGoal.command = gripperCommand;
+                gripperClient.sendGoal(gripperGoal);
+                //gripSuccess = gripperClient.waitForResult();
+                if (gripSuccess){ROS_INFO_NAMED("graspDemo","ACTION WAS EXECUTED SUCCESFULY!!!!!!");}
+                else{ROS_ERROR_NAMED("graspDemo","Gripper Failed To close please override Manually");}
+            
+            }
+        }
+
+       ROS_INFO_NAMED("graspDemo","This attemp reached termination ...");
+       char next_action;
+       std::cout << "To Execute another Grasp Enter Y" << std::endl;
+       std::cin>> next_action;
+       if (next_action == 'Y'){ attemp_grasp = true;}
+       else { attemp_grasp = false;}
     }
-
-    }
-
-    //Now we will use IK and followJointTrajectory action to move the arm to the final grap 
-    //position
-    
-     // Raw pointers are frequently used to refer to the planning kgroup for
-    // improved performance.
-    std::cout << "To move to the grasp Press Enter" << std::endl;
-    std::cin.ignore();
-    std::vector<geometry_msgs::Pose> waypoints;
-    waypoints.push_back(preGrasp);
-    waypoints.push_back(finGrasp);
-    moveit_msgs::RobotTrajectory trajectory;
-    const double jthr= 0;
-    const double eef_step= 0.3;
-    double fraction = move_group.computeCartesianPath(waypoints, eef_step, jthr, trajectory,false);
-    while (fraction <0.8){
-        fraction = move_group.computeCartesianPath(waypoints, eef_step, jthr, trajectory,false);
-    }
-
-    std::cout << "To execute the Grap  Press Enter" << std::endl;
-    std::cin.ignore();
-    ROS_INFO("Openning gripper ..");
-    control_msgs::GripperCommandGoal gripperGoal;
-    control_msgs::GripperCommand gripperCommand;
-    gripperCommand.position = 0.1; 
-    gripperCommand.max_effort = -1; 
-    gripperGoal.command = gripperCommand;
-    gripperClient.sendGoal(gripperGoal);
-    bool gripSuccess=  gripperClient.waitForResult();
-    if (gripSuccess){ROS_INFO("ACTION WAS EXECUTED SUCCESFULY!!!!!!");}
-    else{ ROS_ERROR("EXECUTION FAILED DONT KNOW WHAT TO DO NOW");}
-
-    
-    ROS_INFO("Moving To grasp Location  ..");
-
-    std::cin.ignore();
-    control_msgs::FollowJointTrajectoryGoal trajGoal;
-    trajGoal.trajectory = trajectory.joint_trajectory;
-    armClient.sendGoal(trajGoal);
-    bool trajSuccess=  armClient.waitForResult();
-    if (trajSuccess){ROS_INFO("ACTION WAS EXECUTED SUCCESFULY!!!!!!");}
-    else{ ROS_ERROR("EXECUTION FAILED DONT KNOW WHAT TO DO NOW");}
-
-    std::cout << "To cl Grap  Press Enter" << std::endl;
-    std::cin.ignore();
-
-    ROS_INFO("Closing  gripper ..");
-    gripperCommand.position = 0.02; 
-    gripperCommand.max_effort = -1; 
-    gripperGoal.command = gripperCommand;
-    gripperClient.sendGoal(gripperGoal);
-    gripSuccess = gripperClient.waitForResult();
-    if (gripSuccess){ROS_INFO("ACTION WAS EXECUTED SUCCESFULY!!!!!!");}
-    else{ ROS_ERROR("EXECUTION FAILED DONT KNOW WHAT TO DO NOW");}
-
-
+  
 }
